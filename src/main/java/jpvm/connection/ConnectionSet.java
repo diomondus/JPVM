613e09f6a3cef1ec9d6adb990cf006c5258be516
Dmitry Butilov <acebutilov@gmail.com>
2017-12-23 00:09:10 +0300

    Рефакторит класс в соответствии с Java Code Style

--- src/main/java/jpvm/connection/ConnectionSet.java
+++ src/main/java/jpvm/connection/ConnectionSet.java
@@ -11,61 +11,55 @@ public class ConnectionSet {
     private SendConnectionListNode sendList;
     private RecvConnectionListNode recvListIter;
 
-    public ConnectionSet() {
-        recvList = null;
-        sendList = null;
-    }
-
-    public synchronized RecvConnection
-    lookupRecvConnection(TaskId tid) {
-        RecvConnectionListNode tmp = recvList;
-        while (tmp != null) {
-            if (tmp.conn.tid.equals(tid))
-                return tmp.conn;
-            tmp = tmp.next;
+    public synchronized RecvConnection lookupRecvConnection(TaskId tid) {
+        RecvConnectionListNode list = recvList;
+        while (list != null) {
+            if (list.conn.tid.equals(tid)) {
+                return list.conn;
+            }
+            list = list.next;
         }
         return null;
     }
 
     public synchronized void insertRecvConnection(RecvConnection c) {
-        RecvConnectionListNode nw;
-        nw = new RecvConnectionListNode(c);
-        nw.next = recvList;
-        recvList = nw;
+        RecvConnectionListNode list = new RecvConnectionListNode(c);
+        list.next = recvList;
+        recvList = list;
     }
 
-    public synchronized RecvConnection
-    firstIterRecv() {
+    public synchronized RecvConnection firstIterRecv() {
         recvListIter = recvList;
         if (recvListIter != null)
             return recvListIter.conn;
         return null;
     }
 
-    public synchronized RecvConnection
-    nextIterRecv() {
-        if (recvListIter != null)
+    public synchronized RecvConnection nextIterRecv() {
+        if (recvListIter != null) {
             recvListIter = recvListIter.next;
-        if (recvListIter != null)
+        }
+        if (recvListIter != null) {
             return recvListIter.conn;
+        }
         return null;
     }
 
-    public synchronized SendConnection
-    lookupSendConnection(TaskId tid) {
-        SendConnectionListNode tmp = sendList;
-        while (tmp != null) {
-            if (tmp.conn.tid.equals(tid))
-                return tmp.conn;
-            tmp = tmp.next;
+    public synchronized SendConnection lookupSendConnection(TaskId tid) {
+        SendConnectionListNode list = sendList;
+        while (list != null) {
+            if (list.conn.tid.equals(tid)) {
+                return list.conn;
+            }
+            list = list.next;
         }
         return null;
     }
 
     public synchronized void insertSendConnection(SendConnection c) {
-        SendConnectionListNode nw;
-        nw = new SendConnectionListNode(c);
-        nw.next = sendList;
-        sendList = nw;
+        SendConnectionListNode list;
+        list = new SendConnectionListNode(c);
+        list.next = sendList;
+        sendList = list;
     }
 }
